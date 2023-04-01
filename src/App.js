e58da3f3d04074452e13e64bbe2b43cd50b787f5
MuhammetLvnt <79282877+MuhammetLvnt@users.noreply.github.com>
2023-04-01 13:16:16 +0300

    app.js contextProvider with wrapped up

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import Content from "./components/content";
+import ContentFooter from "./components/ContentFooter";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import { UrlContextProvider } from "./context/UrlContext";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <UrlContextProvider>
+        <section className="todoapp">
+          <Header />
+          <Content />
+          <ContentFooter />/
+        </section>
+        <Footer />
+      </UrlContextProvider>
+    </div>
+  );
 }
 
 export default App;
